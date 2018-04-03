215623bd9216850be62db7edb036714bf165b94b
Cyber-Cucumber <35904235+Cyber-Cucumber@users.noreply.github.com>
2018-04-04 00:24:36 +0300

    9

---
+++ bin/ru/torment/data/sc_jungle_world_water_mud.tsx
@@ -0,0 +1,489 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sc_jungle_world_water_mud" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="../SC Tiles/sc_jungle_world_water_mud.png" width="384" height="384"/>
+ <terraintypes>
+  <terrain name="water" tile="41"/>
+  <terrain name="mud" tile="114"/>
+  <terrain name="dirt" tile="107"/>
+ </terraintypes>
+ <tile id="4" terrain="2,2,2,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="5" terrain="2,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="6" terrain="2,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="7" terrain="2,2,0,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="14" terrain="2,2,2,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15" terrain="2,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="16" terrain="2,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="17" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="18" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="19" terrain="0,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="20" terrain="2,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="21" terrain="2,2,0,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="24" terrain="2,2,2,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="25" terrain="2,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="26" terrain="2,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="27" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="28" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="29" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="30" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="31" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="32" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="33" terrain="0,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="34" terrain="2,2,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="35" terrain="2,2,0,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="36" terrain="2,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="37" terrain="0,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="38" terrain="0,0,2,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="39" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="40" type="water" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="41" type="water" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="42" type="water" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="43" type="water" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="44" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="45" terrain="0,0,0,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="46" terrain="0,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="47" terrain="0,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="50" terrain="2,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="51" terrain="0,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="52" terrain="0,0,2,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="53" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="54" terrain="0,0,0,0">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="55" terrain="0,0,0,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="56" terrain="0,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="57" terrain="0,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="64" terrain="2,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="65" terrain="0,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="66" terrain="0,0,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="67" terrain="0,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="76" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="77" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="78" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="79" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="86" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="87" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="88" terrain="2,2,2,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="89" terrain="2,2,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90" terrain="2,2,1,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="91" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="92" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="93" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="96" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="97" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="98" terrain="2,2,2,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="99" terrain="2,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="101" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="102" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="103" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="104" terrain="1,2,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="105" terrain="2,2,1,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="106" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="107" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="108" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="109" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="110" terrain="2,1,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="111" terrain="1,1,2,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="112" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="113" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="114" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="115" terrain="1,1,1,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="116" terrain="1,1,1,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="117" terrain="1,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="118" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="119" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="122" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="123" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="124" terrain="2,1,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="125" terrain="1,1,2,1">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="126" terrain="1,1,1,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="127" terrain="1,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="128" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="129" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="136" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="137" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="138" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="139" terrain="2,2,2,2">
+  <properties>
+   <property name="isEnable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
